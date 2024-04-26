0b5d48dad8c24c3c8ee9e5e7e640e8d6a375ec34
IgorYudintsev <kakvospitat@gmail.com>
2024-04-26 19:38:46 +0300

    26.04.2024-7:38-MERGE-MASTER

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Igor} from "./Igor";
 
 function App() {
   return (
     <div className="App">
       <div>26.04.2024-7:24-MASTER</div>
       <div>26.04.2024-7:26-MASTER</div>
+        <Igor/>
     </div>
   );
 }
