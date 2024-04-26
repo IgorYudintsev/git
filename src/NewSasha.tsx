1186047d12db7d03a57547df9d70f1d7af90afb7
DilanBars2017 <developeryudintsev@gmail.com>
2024-04-26 19:54:27 +0300

    26.04.2024-7:54-NewSasha

--- src/NewSasha.tsx
+++ src/NewSasha.tsx
@@ -7,6 +7,7 @@ export const NewSasha = (props: Props) => {
     return (
         <div style={{backgroundColor:'yellow'}}>
             <div>26.04.2024-7:53-NewSasha</div>
+            <div>26.04.2024-7:54-NewSasha</div>
         </div>
     );
 };
