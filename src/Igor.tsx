eb261a4ae92c63eba61d56b0574ba9bf26e0d328
IgorYudintsev <kakvospitat@gmail.com>
2024-04-26 19:35:28 +0300

    26.04.2024-7:35-IGOR

---
+++ src/Igor.tsx
@@ -0,0 +1,13 @@
+
+
+type Props = {
+
+};
+export const Igor = (props: Props) => {
+    return (
+        <div style={{backgroundColor:'red'}}>
+            <div>26.04.2024-7:35-IGOR</div>
+        </div>
+
+    );
+};
